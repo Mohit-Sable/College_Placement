00f853c6e0899c70cad8189f24680afaefd2358e
Mohit Sable <mohitsable07@gmail.com>
2026-01-29 19:32:47 +0530

    Modify Vite config to add allowed hosts
    Updated Vite configuration to include allowed hosts.

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,11 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  plugins: [react()],
   server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+    allowedHosts: [
+      "collage-placement.onrender.com"
+    ]
+  }
+});
